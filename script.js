99d6592fc8655afcad6e05efd90fefffbb1d99d9
Sowmindhu <112560899+Sowmindhu@users.noreply.github.com>
2022-12-08 21:30:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+    menu.classList.Remove('fa-times');
+    navbar.classList.toggle('active');
+
+}
